e4434109904711c3394f1ee7c4d7e6407b5187c6
Ramadhani <ramadhanifals@gmail.com>
2024-10-28 06:31:33 +0000

    gs101: Define target kernel path
    Change-Id: Ibd421512b6e46830126382d2da8b4aabfbf2624d

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -16,6 +16,10 @@
 include build/make/target/board/BoardConfigMainlineCommon.mk
 include build/make/target/board/BoardConfigPixelCommon.mk
 
+KERNEL_PATH := device/google/raviole-kernel
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_KERNEL_SOURCE := $(KERNEL_PATH)/kernel-headers
+
 # Should be uncommented after fixing vndk-sp violation is fixed.
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 
